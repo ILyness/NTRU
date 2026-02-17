29a02ad7af2e73bc3575970a63174360680065b4
Indy Lyness <indy.lyness@gmail.com>
2026-02-17 13:12:48 -0600

    Final tweaks

--- demos/alice.py
+++ demos/alice.py
@@ -30,8 +30,7 @@ while True:
     conn.sendall(host_name.encode('utf-8'))
     client_name = conn.recv(128).decode('utf-8')
     slow_print(f"{client_name} would like to talk with you, but {Colors.EVE}Eve{Colors.RESET} is listening.")
-
-    slow_print(f"We will use {Colors.BOLD}NTRUEncrypt{Colors.RESET} with 𝑁 = {N}, 𝑝 = {p}, 𝑞 = {q}, 𝑑 = {d} to encrypt {client_name}'s message.\n")
+    slow_print(f"We will use {Colors.BOLD}NTRUEncrypt{Colors.RESET} with 𝑁 = {N}, 𝑝 = {p}, 𝑞 = {q}, 𝑑 = {d} to encrypt {client_name}'s message.")
     slow_print(f"Before {client_name} can send you a message, we need to give them your public key.")
     slow_print("Here are your generated private keys:")
     h_coeffs = ntruencrypt.createRandomKeys()
@@ -41,7 +40,7 @@ while True:
     slow_print(f"\n{Colors.BOLD}h{Colors.RESET} = {prettify_polynomial(ntruencrypt.h.__str__())}\n", delay=0.02)
     h_str = qnaryCoeffsToBlock(ntruencrypt.h.coeffs, q)
     slow_print("We need to turn your public key into a message we can send. This message is:")
-    slow_print(f"{Colors.GREEN}{repr(h_str)[1:-1]}{Colors.RESET}")
+    slow_print(f"\n{Colors.MESSAGE}{repr(h_str)[1:-1]}{Colors.RESET}\n")
     slow_print(f"Sending {client_name} your public key.")
     conn.sendall(h_str.encode('utf-8'))
 
@@ -49,7 +48,7 @@ while True:
     with Loader(desc=f"Waiting for {client_name}'s message...", end=f"{client_name} has sent you their ciphertext."):
         data = conn.recv(4096)
     ciphertext = data.decode('utf-8')
-    slow_print(f"{Colors.BOLD}Ciphertext{Colors.RESET}: {Colors.GREEN}{repr(ciphertext)[1:-1]}{Colors.RESET}")
+    slow_print(f"{Colors.BOLD}Ciphertext{Colors.RESET}: {Colors.MESSAGE}{repr(ciphertext)[1:-1]}{Colors.RESET}")
     slow_print(f"{Colors.EVE}Eve{Colors.RESET} can see your public key and {client_name}'s ciphertext, but she won't be able to retrieve m.")
     slow_print("However, you can use your private keys to decrypt the message. The decrypted result is:")
     plaintext = decryptMessage(ciphertext, ntruencrypt)
